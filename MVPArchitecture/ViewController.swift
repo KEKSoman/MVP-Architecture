8db13352720af930c37cd518e8be756e3d4c7e9d
KEKSman <bassman7009@gmail.com>
2022-06-12 14:03:15 +0700

    Initial Commit

---
+++ MVPArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPArchitecture
+//
+//  Created by Евгений колесников on 12.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
